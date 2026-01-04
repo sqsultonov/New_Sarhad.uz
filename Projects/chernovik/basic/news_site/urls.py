9cb6e6d87f7c3e58de83bc0cc47b86a69fd562a3
Sardorbek <sardorbeksultonov@gmail.com>
2026-01-04 20:39:39 +0500

    Fix commented line and clean up urlpatterns

--- Projects/chernovik/basic/news_site/urls.py
+++ Projects/chernovik/basic/news_site/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact_page, name='contact'),
     path('elements/', views.elements_page, name='elements'),
-    # 'single-post/' o'rniga dinamik 'post/<slug:slug>/' qo'ydik
+    # 'single-post/' o'rniga dinamik 'post/<slug:slug>/'
     path('post/<slug:slug>/', views.singlepost_page, name='single-post'),
     path('category/<slug:slug>/', views.category_page, name='category'),
   path('post/<slug:slug>/', views.singlepost_page, name='single-post'),
     path('category/<slug:slug>/', views.category_page, name='category'),
-    path('search/', views.search_results, name='search'), # Yangi yo'l
+    path('search/', views.search_results, name='search'),
 ]
